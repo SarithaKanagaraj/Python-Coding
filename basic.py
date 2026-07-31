dc07515bf4d4bdf064f81d4b501150eb21962375
SarithaKanagaraj <62903186+SarithaKanagaraj@users.noreply.github.com>
2026-07-31 18:43:44 -0500

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,11 @@
+# Printing a greeting messsage
+print("Hello, World!")
+
+# Printing my name
+print("My name is Aarav.")
+
+# Printing my favorite hobby
+print("My favorite hobby is basketball.")
+
+# Printing and performing a simple calculation
+print("The result of 3+5 is:",3+5)
---
+++ mydetails.py
@@ -0,0 +1,14 @@
+# Printing my name
+print("My name is Aarav")
+
+# Printing My favorite food
+print("My favorite food is pizza.")
+
+# Printing my favorite animal
+print("My favorite animal is an elephant.")
+
+# Printing the college I want to go to
+print("The college I want to go to is Harvard.")
+
+# Printing my dream job
+print("My dream job is to become a lawyer.")
