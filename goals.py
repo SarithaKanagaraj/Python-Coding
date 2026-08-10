5fc07aa81f3c28e742c9e06b6b7ba3bc0f7dc4d0
SarithaKanagaraj <62903186+SarithaKanagaraj@users.noreply.github.com>
2026-08-10 12:25:41 -0500

    Add files via upload

---
+++ goals.py
@@ -0,0 +1,15 @@
+import keyword
+
+# Print all the python keywords
+print("Python Keywords are\n")
+print(keyword.kwlist)
+
+# Ask for details
+name=input("Enter Your Name Here:")
+goal=input("Enter Your Goal Here:")
+target_month=input("Enter Your Target Month Here:")
+
+# Format Output
+print("Name:",name,"\n")
+print("Goal::",goal,"\n")
+print("Target Month:",target_month,"\n")
