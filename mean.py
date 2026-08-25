6953dd00711bc7f0f771ecc034c5ca7cfb78a2ae
SarithaKanagaraj <62903186+SarithaKanagaraj@users.noreply.github.com>
2026-08-25 17:50:09 -0500

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,16 @@
+mean1=38
+wrong_number=36
+right_number=56
+total_numbers=40
+
+# sum of 40 numbers
+sum=mean1*total_numbers
+print("Initial sum:",sum)
+
+# Correct sum
+sum2=sum-wrong_number+right_number
+print("Correct sum is:",sum2)
+
+# Correct mean
+right_mean=sum2/total_numbers
+print("Correct mean value is:",right_mean)
